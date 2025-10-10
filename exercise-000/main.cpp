80ef7a58d92627664ef838776ed0425e260fc7ec
Hubertus97 <157050121+Hubertus97@users.noreply.github.com>
2025-10-10 13:48:16 +0200

    Aufgabe1 angefangen

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -12,7 +12,20 @@ int main(int argc, char **argv) {
      */
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
 
-    /* INSERT YOUR CODE HERE */
+    /*for (int i = 0 ; i< argc; i++){
+        fmt::println("argc:{}, argument{} (Adresse:{})",i, argv[i],fmt::ptr(argv[i]));
+    }*/
+
+    fmt::println("------Aufgabe1------");
+
+    int x =10;
+    int* p = &x;
+
+    fmt::println("Wert von x:{}, Adresse von x:{}",x,fmt::ptr(&x));
+    
+    fmt::println("Wert auf den Zeiger p zeigt:{}",*p);
+
+    fmt::println("Adresse die in p gespeichert ist:{}",fmt::ptr(p));
 
     return 0; /* exit gracefully*/
 }
