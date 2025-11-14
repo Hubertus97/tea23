3a2b68afcc39d9ae64ad72231209a4654fd5001f
Hubertus97 <157050121+Hubertus97@users.noreply.github.com>
2025-11-14 15:28:11 +0100

    Added switching between states

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,5 +1,6 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <iostream>
 
 enum class GarageState{
     FahreHoch,
@@ -24,7 +25,80 @@ auto main(int argc, char** argv) -> int
 
     while (true)
     {
-        /* code */
+        // --- Simulation der Eingabe ---
+        // (Wir lesen die Eingaben am Anfang jedes Schleifendurchlaufs)
+        fmt::println("\nAktueller Status: {}", CurrentState); // Zeigt den aktuellen Status
+        fmt::println("Drücke Enter für 'Taste', 'o' für 'Oben', 'u' für 'Unten'");
+        
+        char input = std::cin.get();
+        if(input != '\n') { // Puffer leeren, falls nicht nur Enter gedrückt wurde
+            std::cin.ignore(10000, '\n'); 
+        }
+        // --- Ende Simulation ---
+
+
+        // --- Start der State Machine Logik ---
+        switch (CurrentState)
+        {
+        case GarageState::FahreHoch:
+            fmt::println("Tor fährt hoch");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::StopAufDemWegNachOben;
+            }
+            else if (input == 'o')
+            {
+                CurrentState = GarageState::GaragenTorOffen;
+            }
+            break;
+
+        case GarageState::FahreRunter:
+            fmt::println("Tor fährt runter");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::StopAufDemWegNachUnten;
+            }
+            else if (input == 'u')
+            {
+                CurrentState = GarageState::GaragenTorZu;
+            }
+            break;
+
+        case GarageState::GaragenTorOffen:
+            fmt::println("Tor ist offen");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::FahreRunter;
+            }
+            break;
+
+        case GarageState::GaragenTorZu:
+            fmt::println("Tor ist zu");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::FahreHoch;
+            }
+            break;
+
+        case GarageState::StopAufDemWegNachOben:
+            fmt::println("Stop auf dem Weg nach oben");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::FahreRunter;
+            }
+            break;
+
+        case GarageState::StopAufDemWegNachUnten:
+            fmt::println("Stop auf dem Weg nach unten");
+            if (input == '\n')
+            {
+                CurrentState = GarageState::FahreHoch;
+            }
+            break;
+        
+        default:
+            break;
+        }
     }
 
 
